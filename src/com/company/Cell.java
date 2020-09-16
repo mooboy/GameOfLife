5db08760e0e5539277772f2274468e293c7eb167
prabsan <62865286+prabsan@users.noreply.github.com>
2020-09-15 19:08:50 -0700

    Update Cell.java

--- src/com/company/Cell.java
+++ src/com/company/Cell.java
@@ -1,32 +1,29 @@
 package com.company;
 
+// Cell class determines the state of each cell on the grid (alive/dead) as well as the position on the grid
 public class Cell {
     Boolean state;
     int xPosition;
     int yPosition;
 
+
     public Cell() {
 
     }
 
+    // Cell constructor takes x, y, and state of cell
     public Cell(int x, int y, boolean isAlive) {
         this.xPosition = x;
         this.yPosition = y;
         this.state = isAlive;
     }
 
+    // method returns the state of the cell
     public Boolean getState() {
         return this.state;
     }
 
-    public int getXPosition() {
-        return this.xPosition;
-    }
-
-    public int getYPosition() {
-        return this.yPosition;
-    }
-
+    // method returns state and position of cell
     public String toString() {
         return "IsAlive: " + this.state + " | " + "X-Pos: " + this.xPosition + "  |  Y-Pos:" + this.yPosition;
     }
